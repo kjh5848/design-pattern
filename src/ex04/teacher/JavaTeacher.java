5159026f8d281af849c2e4cdda082dbad50bbabf
kjh5848 <kjh5848@gmail.com>
2024-05-28 10:23:53 +0900

    파이썬 강의하기 수정

--- src/ex04/teacher/JavaTeacher.java
+++ src/ex04/teacher/JavaTeacher.java
@@ -1,6 +1,8 @@
 package ex04.teacher;
 
 public class JavaTeacher {
+
+    //이렇게 묶는걸 템플릿 메서드 패턴이라고 한다.
     public void 수업하기() {
         입장하기();
         출석부르기();
@@ -14,16 +16,13 @@ public class JavaTeacher {
 
     private void 출석부르기() {
         System.out.println("출석부르기");
-
     }
 
     private void 강의하기() {
-        System.out.println("자바 강의하기");
-
+        System.out.println("파이썬 강의하기");
     }
 
     private void 퇴장하기() {
         System.out.println("퇴장하기");
-
     }
 }
